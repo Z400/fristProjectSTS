8633e6da71244649ebf47d501cb70c083659f386
VIANA <matheusgoldship@gmail.com>
2023-10-03 12:34:04 -0300

    Created H2 DATABASE

--- src/main/java/vianaweb/course/resources/UserResouce.java
+++ src/main/java/vianaweb/course/resources/UserResouce.java
@@ -1,7 +1,6 @@
 package vianaweb.course.resources;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
