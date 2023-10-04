55293a118fe88e526b046a0da6d36407a33455c2
VIANA <matheusgoldship@gmail.com>
2023-10-04 13:04:46 -0300

    Order Status Enum

--- src/main/java/vianaweb/course/entitie/Order.java
+++ src/main/java/vianaweb/course/entitie/Order.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import vianaweb.course.enums.OrderStatus;
 
 @Entity
 @Table (name = "tb_order")
@@ -33,14 +34,18 @@ public class Order implements Serializable {
 	@JoinColumn (name = "client_id")
 	private User client;
 	
+	
+	private Integer orderStatus;
+	
 	Order () {
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 	
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 	
@@ -63,6 +68,18 @@ public class Order implements Serializable {
 
 	public void setDate(Instant moment) {
 		this.moment = moment;
+	}
+
+		 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+		
 	}
 
 	public User getClient() {
